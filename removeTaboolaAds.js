c929fd3cc0a404870b5d96e0a58e0822f75183ff
Wayne Dupree <wdupree@hotmail.com>
2019-06-13 10:28:11 -0500

    Update scanned tags, add infinite reading list removal
    Removed checking for all tags with taboola classes to only checking div and p tags. Removed infinite scrolling stories for Gizmodo.

--- removeTaboolaAds.js
+++ removeTaboolaAds.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Remove Taboola Ads
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
-// @description  Remove Taboola Ads from sites.
+// @version      0.2.2
+// @description  Remove Taboola Ads from sites & javascript reading lists.
 // @updateURL    https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js?token=ABNSKDIZHWTUIS4DD3YFNSK4ZM5YY
 // @downloadURL  https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js?token=ABNSKDIZHWTUIS4DD3YFNSK4ZM5YY
 // @author       Wayne Dupree
@@ -14,8 +14,10 @@
     'use strict';
 
     var taboolaClasses = [
-        "[id*='taboola']",
-        "[class*='taboola']"
+        "[id*='taboola']" +
+	"div[class*='taboola']",
+        "p[class*='taboola']",
+	"[class*='js_reading-list reading-list']"
 	];
 
     taboolaClasses.forEach(function(taboolaClass){
@@ -23,10 +25,10 @@
         taboolaElement.forEach(function(taboolaElement){
             GM_addStyle(
                 taboolaClass + " {" +
-                "display: none !important;" +
-                "visibility: hidden !important;" +
-				"width: 0px !important;" +
-				"height: 0px !important;" +
+                	"display: none !important;" +
+                	"visibility: hidden !important;" +
+			"width: 0px !important;" +
+			"height: 0px !important;" +
                 "}"
             );
             console.info("Remove Taboola Ads hid element [Identification: " + taboolaClass + "].");
