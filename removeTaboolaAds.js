ba59d1acde098641c092219c1598dd2c0ca74992
Wayne Dupree <wdupree@hotmail.com>
2019-06-13 12:00:05 -0500

    Remove taboola elements and better element naming.
    Change to remove the taboola elements instead of hiding them and better identification of removed elements.

--- removeTaboolaAds.js
+++ removeTaboolaAds.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Remove Taboola Ads
 // @namespace    http://tampermonkey.net/
-// @version      0.2.2
-// @description  Remove Taboola Ads from sites & javascript reading lists.
-// @updateURL    https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js?token=ABNSKDIZHWTUIS4DD3YFNSK4ZM5YY
-// @downloadURL  https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js?token=ABNSKDIZHWTUIS4DD3YFNSK4ZM5YY
+// @version      0.2.3
+// @description  Remove Taboola Ads from sites.
+// @updateURL    https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js?token=ABNSKDNNAOOHXTNPZEYXGAC5AJWAI
+// @downloadURL  https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js?token=ABNSKDNNAOOHXTNPZEYXGAC5AJWAI
 // @author       Wayne Dupree
 // @match        http*://*/*
 // @grant    	 GM_addStyle
@@ -14,24 +14,21 @@
     'use strict';
 
     var taboolaClasses = [
-        "[id*='taboola']" +
-	"div[class*='taboola']",
+        "[id*='taboola']",
+        "div[class*='taboola']",
         "p[class*='taboola']",
-	"[class*='js_reading-list reading-list']"
+        "[class*='js_reading-list reading-list']",
+        "[class*='related-articles-block']"
 	];
 
     taboolaClasses.forEach(function(taboolaClass){
         var taboolaElement = document.querySelectorAll(taboolaClass);
         taboolaElement.forEach(function(taboolaElement){
-            GM_addStyle(
-                taboolaClass + " {" +
-                	"display: none !important;" +
-                	"visibility: hidden !important;" +
-			"width: 0px !important;" +
-			"height: 0px !important;" +
-                "}"
-            );
-            console.info("Remove Taboola Ads hid element [Identification: " + taboolaClass + "].");
+            taboolaElement.remove();
+            var tagName = taboolaElement.localName +
+                ((taboolaElement.id) ? '#' + taboolaElement.id : '') +
+                ((taboolaElement.className) ? '.' + taboolaElement.className : '');
+            console.info(`Remove Taboola Ads removed element [Identification: ${tagName}].`);
         });
     });
 })();
