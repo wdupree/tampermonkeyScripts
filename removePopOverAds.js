1cae1e03f4c79583ef182d2fe8f54e33abe770d6
Wayne Dupree <wdupree@hotmail.com>
2019-06-17 08:02:53 -0500

    Add Telegraph, clean up CNN
    Added changes to catch the pop-over on the Telegraph website and cleaned up CNN's to also remove the overflow on the BODY tag.

--- removePopOverAds.js
+++ removePopOverAds.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Remove Pop Over Ads
 // @namespace    http://tampermonkey.net/
-// @version      0.2.3
+// @version      0.2.4
 // @description  Remove add popovers from ComingSoon.net CNN, NBC & Windows Central.
-// @updateURL    https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removePopOverAds.js?token=ABNSKDJX6OUBRYBXJWKIUQK4ZM5SC
-// @downloadURL  https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removePopOverAds.js?token=ABNSKDJX6OUBRYBXJWKIUQK4ZM5SC
+// @updateURL    https://gist.githubusercontent.com/wdupree/70c0989a67763b5a65783a50089f9411/raw/7885b0510b3c387f39c086660d442a10c42ebaff/removePopOverAds.js
+// @downloadURL  https://gist.githubusercontent.com/wdupree/70c0989a67763b5a65783a50089f9411/raw/7885b0510b3c387f39c086660d442a10c42ebaff/removePopOverAds.js
 // @author       Wayne Dupree
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -12,6 +12,7 @@
 // @match        *://www.cnn.com/*
 // @match        *://www.nbcnews.com/*
 // @match        *://www.windowscentral.com/*
+// @match        *://www.telegraph.co.uk/*
 // ==/UserScript==
 
 (function() {
@@ -19,7 +20,8 @@
 
     var cssPopOverSelectors = 'div.bx-slab,' + // CNN
                               'div[style*="background-color: rgba(0, 0, 0, 0"],' + // NBC News, Coming Soon
-                              'div.swal-overlay'; // Windows Central
+                              'div.swal-overlay,' + // Windows Central
+                              'div.adblocker-message.component'; // Telegraph
 
     waitForKeyElements (cssPopOverSelectors, elementFoundCallbackFunction);
 
@@ -40,8 +42,20 @@
                }
                console.info(`Remove Pop Over Ads removed element [Identification: ${tagName}].`);
 
-               $('html').removeAttr('style');
-               $('body').removeAttr('style');
+               if ('html[style*="overflow"]') {
+                   $('html').removeAttr('style');
+                   console.info(`Remove Pop Over Ads removed the style attribute from HTML tag.`);
+               }
+
+               if ('body[style*="overflow"]') {
+                   $('body').removeAttr('style');
+                   console.info(`Remove Pop Over Ads removed the style attribute from BODY tag.`);
+               }
+
+               if ($('body').hasClass('bx-client-overlay')) {
+                   $('body').removeClass('bx-client-overlay');
+                   console.info(`Remove Pop Over Ads removed the bx-client-overlay class from the BODY tag.`);
+               }
            });
     }
 })();
