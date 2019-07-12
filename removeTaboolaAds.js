2ed7b9838e4d6d80cee5abb75abe42a448164bde
Wayne Dupree <wdupree@hotmail.com>
2019-07-12 14:20:27 -0500

    Add in removal of Dianomi ads
    Added the removal of Dianomi ads (example site: https://www.reuters.com/article/us-storm-barry-pumps/storm-barry-threatens-to-overwhelm-new-orleans-century-old-pump-system-idUSKCN1U7278).

--- removeTaboolaAds.js
+++ removeTaboolaAds.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Remove Taboola Ads
 // @namespace    http://tampermonkey.net/
-// @version      0.2.4
+// @version      0.3.0
 // @description  Remove Taboola Ads from sites.
 // @updateURL    https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js
 // @downloadURL  https://raw.githubusercontent.com/wdupree/tampermonkeyScripts/master/removeTaboolaAds.js
 // @author       Wayne Dupree
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @match        http*://*/*
 // @grant    	 GM_addStyle
 // ==/UserScript==
@@ -31,4 +33,20 @@
             console.info(`Remove Taboola Ads removed element [Identification: ${tagName}].`);
         });
     });
+
+    var cssPopOverSelectors = 'a[href*="dianomi"],' + // Dianomi Footer
+                              'img[src*="reutersmedia"]'; // Dianomi Header
+
+    waitForKeyElements (cssPopOverSelectors, elementFoundCallbackFunction);
+    function elementFoundCallbackFunction ($htmlElements) {
+           $htmlElements.each(function(index, element){
+               if (element.localName == 'a') {
+                   $(element).closest('span').hide();
+                   console.info(`Remove Taboola Ads removed a Dianomi footer element.`);
+               } else{
+                   $(element).hide();
+                   console.info(`Remove Taboola Ads removed a Dianomi header element.`);
+               }
+           });
+    }
 })();
